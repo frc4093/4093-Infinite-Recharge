f64bf85791388293a6a0b2d9ed10ace9be922fa7
Frc4093 <54691276+frc4093@users.noreply.github.com>
2020-01-25 15:01:06 -0500

    gyro stuff

--- src/main/java/frc/robot/commands/XDrive.java
+++ src/main/java/frc/robot/commands/XDrive.java
@@ -51,7 +51,7 @@ public class XDrive extends Command {
 
         }
     }
-
+    
     // Make this return true when this Command no longer needs to run execute()
     @Override
     protected boolean isFinished() {
