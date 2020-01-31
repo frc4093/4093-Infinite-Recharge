09fa4b35aa8c42709c777bc8390fc109b26f2441
Jayden <48694012+JaydenN117@users.noreply.github.com>
2020-01-31 16:53:01 -0500

    shoot with PID small tweak

--- src/main/java/frc/robot/commands/ShootWithPID.java
+++ src/main/java/frc/robot/commands/ShootWithPID.java
@@ -57,11 +57,13 @@ public class ShootWithPID extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.shooter.setShooter(ControlMode.PercentOutput, 0);
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     @Override
     protected void interrupted() {
+        end();
     }
 }
