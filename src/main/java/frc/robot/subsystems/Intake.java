f4d75e89c2bb7112f2d3f8653494e6c7e4244c98
Jayden <48694012+JaydenN117@users.noreply.github.com>
2020-01-31 20:16:59 -0500

    only controlling intake wheels until I know how indexer will need to be programmed

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -71,12 +71,10 @@ conveyor = new WPI_TalonFX(5);
     //basic functionality(for now)
     public void feed(){
         intakeWheels.set(-.5);
-        conveyor.set(-.5);
     }
 
     public void eject(){
         intakeWheels.set(.7);
-        conveyor.set(.5);
     }
     public void stop(){
         intakeWheels.set(0);
