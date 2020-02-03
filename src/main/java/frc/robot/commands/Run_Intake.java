6f4d7b521000385a9dda1dc151b736d524639fa3
Jayden <48694012+JaydenN117@users.noreply.github.com>
2020-02-02 21:25:36 -0500

    intake

--- src/main/java/frc/robot/commands/Run_Intake.java
+++ src/main/java/frc/robot/commands/Run_Intake.java
@@ -43,6 +43,7 @@ public class Run_Intake extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
+        Robot.intake.feed();
     }
 
     // Make this return true when this Command no longer needs to run execute()
@@ -54,6 +55,7 @@ public class Run_Intake extends Command {
     // Called once after isFinished returns true
     @Override
     protected void end() {
+        Robot.intake.stop();
     }
 
     // Called when another command which requires one or more of the same
